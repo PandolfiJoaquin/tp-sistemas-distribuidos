185c713553a20bda7820c0bae0e0a91efbafe338
PandolfiJoaquin <jpandolfi@fi.uba.ar>
2025-04-13 16:59:43 -0300

    feat: Q1

--- server/common/model.go
+++ server/common/model.go
@@ -1,8 +1,9 @@
 package common
 
 type Movie struct {
-	ID    string `json:"id"`
-	Title string `json:"title"`
-	Year  int    `json:"year"`
-	Genre string `json:"genre"`
+	ID                  string   `json:"id"`
+	Title               string   `json:"title"`
+	Year                int      `json:"year"`
+	Genre               string   `json:"genre"`
+	ProductionCountries []string `json:"production_countries"`
 }
