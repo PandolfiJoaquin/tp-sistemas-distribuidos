b9b88317c5f2192e526ade1be73334667bd90197
PandolfiJoaquin <jpandolfi@fi.uba.ar>
2025-04-17 11:04:56 -0300

    Add common functions

--- server/common/utils.go
+++ server/common/utils.go
@@ -10,10 +10,35 @@ func Filter[T any](slice []T, predicate func(T) bool) []T {
 	return result
 }
 
-func Mapped[T, U any](slice []T, mapper func(T) U) []U {
+func First[T any](slice []T, predicate func(T) bool) *T {
+	for _, item := range slice {
+		if predicate(item) {
+			return &item
+		}
+	}
+	return nil
+}
+
+func Map[T, U any](slice []T, mapper func(T) U) []U {
 	var result []U
 	for _, item := range slice {
 		result = append(result, mapper(item))
+	}
+	return result
+}
+
+func Sum[T uint32 | int32 | int](slice []T) int {
+	sum := 0
+	for _, item := range slice {
+		sum += int(item)
+	}
+	return sum
+}
+
+func Flatten[T any](slices [][]T) []T {
+	var result []T
+	for _, slice := range slices {
+		result = append(result, slice...)
 	}
 	return result
 }
