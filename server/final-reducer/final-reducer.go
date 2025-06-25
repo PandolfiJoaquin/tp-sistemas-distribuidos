2c5c6611df1cd0e8b7dc3fc416ac69ed12c809e8
Francisco Juarez <fjuarez@fi.uba.ar>
2025-06-24 23:25:34 -0300

    fix: final reducer logs

--- server/final-reducer/final-reducer.go
+++ server/final-reducer/final-reducer.go
@@ -132,6 +132,8 @@ type connection struct {
 }
 
 func NewFinalReducer(queryNum int, rabbitUser, rabbitPass string, amtOfShards int) (*FinalReducer, error) {
+	slog.Info("starting final reducer", slog.Int("query number", queryNum))
+
 	middleware, err := middleware.NewMiddleware(rabbitUser, rabbitPass, rabbitHost)
 	if err != nil {
 		return nil, fmt.Errorf("error creating middleware: %w", err)
@@ -220,16 +222,12 @@ func (r *FinalReducer) Start() {
 
 	switch r.queryNum {
 	case 2:
-		slog.Info("starting final reducer for query 2")
 		r.startReceivingQ2(ctx)
 	case 3:
-		slog.Info("starting final reducer for query 3")
 		r.startReceivingQ3(ctx)
 	case 4:
-		slog.Info("starting final reducer for query 4")
 		r.startReceivingQ4(ctx)
 	case 5:
-		slog.Info("starting final reducer for query 5")
 		r.startReceivingQ5(ctx)
 	default:
 		slog.Error("query number not found", slog.Int("query number", r.queryNum))
@@ -307,7 +305,6 @@ func startReceiving[T common.Stringer](
 				finishAndSendBatch(session.SessionId)
 				freddyFazbear.deleteClient(session.SessionId)
 				transaction.Do(DeleteClientOp, session.SessionId)
-				slog.Info("deleted client session", slog.String("clientID", session.SessionId))
 			}
 
 			freddyFazbear.save(transaction)
@@ -650,5 +647,5 @@ func (r *FinalReducer) isBlacklisted(clientID string) bool {
 func (r *FinalReducer) deleteClient(clientID string) {
 	delete(r.Sessions, clientID)
 	r.BlacklistedClients[clientID] = time.Now().Unix()
+	slog.Info("deleted client session", slog.String("clientID", clientID))
 }
-
